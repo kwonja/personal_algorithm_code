658c2478465565de025fb04cc5f6539478b2036b
kwonja <kownja87@gmail.com>
2023-03-08 23:11:36 +0900

    이진탐색nlogn

--- kwonja/릿코드3.08.cpp
+++ kwonja/릿코드3.08.cpp
@@ -1,22 +1,36 @@
+typedef long long ll;
 class Solution {
 public:
-	long long minimumTime(vector<int>& time, int totalTrips) {
-		int minitime = 1;
-		int total = 0;
-
-		while (1)
+	int func1(vector<int>& time, int totalTrips, ll mid)
+	{
+		ll total = 0;
+		for (ll i = 0; i < time.size(); i++)
 		{
-			total = 0;
-			for (int i = 0; i < time.size(); i++)
-			{
-				total += (minitime / time[i]);
-			}
-			if (totalTrips <= total)
-			{
-				break;
-			}
-			minitime++;
+			total += (mid / time[i]);
 		}
-		return minitime;
+		if (total < totalTrips) return 0;
+		else return 1;
+	}
+
+	long long minimumTime(vector<int>& time, int totalTrips) {
+
+		ll s = 0;
+		ll e = (ll)totalTrips*(ll)time[0];
+		ll mid = (e + s) / 2;
+		ll answer = 0;
+		while (s <= e)
+		{
+			if (func1(time, totalTrips, mid))
+			{
+				answer = mid;
+				e = mid - 1;
+			}
+			else
+			{
+				s = mid + 1;
+			}
+			mid = (e + s) / 2;
+		}
+		return answer;
 	}
 };
