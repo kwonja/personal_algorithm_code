05f492c19716bb81b5fc1cdc78fcfb7212ef0459
kwonja <kownja87@gmail.com>
2023-08-06 15:00:00 -0300

    2023.8.6 재귀

---
+++ backjoon/[11]재귀/종이의 개수.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+int arr[7000][7000] = { 0, };
+
+int cnt1 = 0;
+int cnt2 = 0;
+int cnt3 = 0;
+
+bool checkpaper(int n, int r, int c)
+{
+	int check = arr[r][c];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (check != arr[r + i][c + j])
+			{
+				return false;
+			}
+		}
+	}
+	return true;
+}
+void dividepaper(int n, int r, int c)
+{
+	if (checkpaper(n, r, c))
+	{
+		if (arr[r][c] == -1)cnt1++;
+		if (arr[r][c] == 0)cnt2++;
+		if (arr[r][c] == 1)cnt3++;
+		return;
+	}
+	int div = n / 3;
+
+	for (int i = 0; i < n; i = i + div)
+	{
+		for (int j = 0; j < n; j = j + div)
+		{
+			dividepaper(div, r + i, c + j);
+		}
+	}
+}
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cin >> arr[i][j];
+		}
+	}
+	dividepaper(n, 0, 0);
+
+	cout << cnt1 << "\n";
+	cout << cnt2 << "\n";
+	cout << cnt3 << "\n";
+
+
+}
