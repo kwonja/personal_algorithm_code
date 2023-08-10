d5bfa99d2b8ecc39a4c592672c7fa2795167c428
kwonja <kownja87@gmail.com>
2023-08-10 18:57:16 +0900

    23.8.10 재귀

---
+++ backjoon/[11]재귀/Z.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int funz(int n, int r, int c)
+{
+	if (n == 0)return 0;
+	int half = 1 << (n - 1);
+	if (r < half && c < half) return funz(n - 1, r, c);
+	if (r < half && c >= half) return half * half + funz(n - 1, r, c - half);
+	if (r >= half && c < half) return 2 * half * half + funz(n - 1, r - half, c);
+	if (r >= half && c >= half) return 3 * half * half + funz(n - 1, r - half, c - half);
+
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n, r, c;
+	cin >> n >> r >> c;
+	cout << funz(n, r, c);
+
+	return 0;
+}
