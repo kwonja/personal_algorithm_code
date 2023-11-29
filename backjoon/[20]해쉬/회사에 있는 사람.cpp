91204e9fc1fdfb8fe86e42847110016a6fe5108c
kwonja <kownja87@gmail.com>
2023-11-29 21:08:55 +0900

    그외 풀이 추가

--- backjoon/[20]해쉬/회사에 있는 사람.cpp
+++ backjoon/[20]해쉬/회사에 있는 사람.cpp
@@ -30,3 +30,24 @@ int main()
 
 //map보다 set이 메모리적으로 효율이 좋기때문에
 //set을 사용하는 방법도 좋아 보인다.
+#include <bits/stdc++.h>
+using namespace std;
+
+unordered_set<string> s;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	while (n--) {
+		string name, log;
+		cin >> name >> log;
+		if (log == "enter") s.insert(name);
+		else s.erase(name);
+	}
+	vector<string> ans(s.begin(), s.end());
+	sort(ans.begin(), ans.end(), greater<string>());
+	for (auto x : ans) cout << x << '\n';
+}
