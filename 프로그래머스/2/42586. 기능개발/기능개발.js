b1fd5d3f4e3c3c3540dd6a93ac134ee18e858911
KwonSungMin <42410000+kwonja@users.noreply.github.com>
2024-06-07 17:18:23 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,25 +1,19 @@
 function solution(progresses, speeds) {
-    let answer = [];
-    console.log(progresses);
-    
-    
-    while(progresses.length>0)
+    let answer=[0];
+    let days = progresses.map( (progress,index)=>{
+        return Math.ceil( (100-progress)/speeds[index]);   
+    })
+    let maxDay=days[0];
+    for(let i=0, j=0; i<progresses.length; i++)
     {
-        let popcount=0;
-        for(let i=0;i<progresses.length;i++)
+        if(days[i]<=maxDay)
         {
-            progresses[i]+=speeds[i];
+           answer[j]++;
         }
-       
-        while(progresses[0]>=100)
+        else
         {
-             progresses.shift();
-             speeds.shift();
-             popcount++;
-        }
-        if(popcount>0)
-        {
-            answer.push(popcount);
+            answer[++j]=1;
+            maxDay=days[i];
         }
     }
     return answer;
