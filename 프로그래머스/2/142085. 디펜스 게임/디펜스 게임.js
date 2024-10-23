7b1a21a1489c388e86e4d0904f8415bb6147b27c
KwonSungMin <42410000+kwonja@users.noreply.github.com>
2024-10-23 09:27:44 +0900

    [level 2] Title: 디펜스 게임, Time: 6467.08 ms, Memory: 125 MB -BaekjoonHub

---
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
@@ -0,0 +1,51 @@
+
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+function solution(n, k, enemy) {
+    let lt = 0, rt = enemy.length;
+
+    while(lt <= rt) {
+        let mid = Math.floor((lt+rt) / 2);
+        if(check(n, k, mid, enemy)) lt = mid+1;
+        else rt = mid - 1;
+    }
+
+    return lt - 1;
+}
+
+const check = (n, k, mid, enemy) => {
+    if (mid <= k) return true;
+
+    let t = enemy.slice(0, mid).sort((a, b) => b - a);
+    let sum = 0;
+
+    for (let i = k; i < t.length; i++) {
+        sum += t[i];
+        if (sum > n) return false;
+    }
+    return true;
+
+}
