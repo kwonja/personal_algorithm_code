2f97f9258f11de3b302eae8372acebcddabd8456
KwonSungMin <42410000+kwonja@users.noreply.github.com>
2024-10-23 09:41:48 +0900

    [level 2] Title: 디펜스 게임, Time: 7208.09 ms, Memory: 125 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
@@ -1,51 +1,32 @@
-
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-21
-22
-23
-24
-25
-26
+//k에 따라서 정렬을 계속하면 시간이 너무 길어짐
+// enemy길이를 이분탐색으로 라운드를 세면  logn의 시간으로 짧게 구할수 있다.
 function solution(n, k, enemy) {
-    let lt = 0, rt = enemy.length;
-
-    while(lt <= rt) {
-        let mid = Math.floor((lt+rt) / 2);
-        if(check(n, k, mid, enemy)) lt = mid+1;
-        else rt = mid - 1;
+    
+    let left=0, right = enemy.length;
+    
+    while(left <= right)
+    {
+        let mid = Math.floor((left + right)/2);
+        //mid 라운드가 최대인지 판별해야함
+        if(check(n,k,mid,enemy))
+        {
+              left=mid+1;      
+        }
+        else right= mid-1;
+        
     }
-
-    return lt - 1;
+    
+    return left-1;
 }
 
-const check = (n, k, mid, enemy) => {
-    if (mid <= k) return true;
-
-    let t = enemy.slice(0, mid).sort((a, b) => b - a);
-    let sum = 0;
-
-    for (let i = k; i < t.length; i++) {
-        sum += t[i];
-        if (sum > n) return false;
+function check(n,k,mid,enemy)
+{
+    let e = enemy.slice(0,mid).sort( (a,b)=> b-a)
+    let sum=0;
+    for(let i=k;i<e.length;i++)
+    {
+        sum +=e[i];
+        if(sum > n) return false;
     }
     return true;
-
 }
