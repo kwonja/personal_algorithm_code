98eab1cbbe0e522ad2371414ba1be553a873aadb
KwonSungMin <42410000+kwonja@users.noreply.github.com>
2025-05-08 23:13:34 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 13.50 ms, Memory: 56.9 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.js
@@ -1,19 +1,29 @@
 function solution(sequence) {
-    let n = sequence.length;
-    let maxPulse1 = -Infinity; // 1로 시작하는 펄스 수열의 최대 합
-    let maxPulse2 = -Infinity; // -1로 시작하는 펄스 수열의 최대 합
-
-    let currentPulse1 = 0; // 1로 시작하는 현재 펄스 수열의 합
-    let currentPulse2 = 0; // -1로 시작하는 현재 펄스 수열의 합
-
-    for (let i = 0; i < n; i++) {
-        let sign = (i % 2 === 0) ? 1 : -1; // 짝수 인덱스이면 1, 홀수 인덱스이면 -1
-        currentPulse1 = Math.max(sequence[i] * sign, currentPulse1 + sequence[i] * sign);
-        currentPulse2 = Math.max(sequence[i] * -sign, currentPulse2 + sequence[i] * -sign);
-
-        maxPulse1 = Math.max(maxPulse1, currentPulse1);
-        maxPulse2 = Math.max(maxPulse2, currentPulse2);
-    }
-
-    return Math.max(maxPulse1, maxPulse2);
+    // 50만이기때문에 이중포문을 쓰거나 하면 무조건 통과하지 못한다.
+    // 힌트 : 연속 부분 수열의 합 -> 이전값들이 현재값보다 작다면? -> 굳이 더할 필요가 없음, 최대값을 가지는 연속수열을 다시 시작하는게 맞다..!
+    
+    
+    //대신 펄스는 바뀌기 때문에 2가지 경우를 확인해줘야함!
+    
+    let maxPurse1 = -Infinity;
+    let maxPurse2 = -Infinity;
+    
+    
+    let sum1=0;
+    let sum2=0;
+    for(let i=0;i<sequence.length;i++)
+        {
+            let purse1= i % 2 === 0 ? 1:-1;
+            let purse2= i % 2 === 0 ? -1:1;
+            
+            sum1 = Math.max(sequence[i] * purse1 , sum1 + sequence[i] * purse1);
+            sum2 = Math.max(sequence[i] * purse2 , sum2 + sequence[i] * purse2);
+        
+            maxPurse1 = Math.max(maxPurse1,sum1);
+            maxPurse2 = Math.max(maxPurse2,sum2);
+        }
+    
+    return Math.max(maxPurse1,maxPurse2);
 }
+
+
