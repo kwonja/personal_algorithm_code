a50473beaf7ab4876bb4c5201f6f64a5bed86068
KwonSungMin <42410000+kwonja@users.noreply.github.com>
2025-05-18 19:55:40 +0900

    [Gold V] Title: 평범한 배낭, Time: 400 ms, Memory: 91212 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.js
@@ -1,33 +1,45 @@
+/**
+ * 골드 5
+ * 배낭 문제
+ */
+
 const fs = require("fs");
 const input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
 
+// 최대 무게는 7이다
+// 첫번째 배낭을 선택했다고 가정하고, 6,7일때 선택할수 있고, 그게 해당 배낭 가치의 최대값이 된다.
+
 let [n, k] = input[0].trim().split(" ").map(Number);
-let item=[];
-for(let i=0;i<n;i++){
-    let [weight, value] = input[i+1].trim().split(" ").map(Number);
-    item.push({weight, value});
+
+let arr = input
+  .slice(1, n + 1)
+  .map((inner) => inner.trim().split(" ").map(Number));
+
+//[0] 무게
+//[1] 가치
+
+let dp = new Array(n + 1);
+for (let i = 0; i < n + 1; i++) {
+  dp[i] = new Array(k + 1).fill(0);
 }
-let arr = new Array(n+1);
-for(let i=0;i<=n;i++){
-    arr[i] = new Array(k+1).fill(0);
+
+//물건탐색
+for (let i = 1; i <= n; i++) {
+  for (let j = 0; j <= k; j++) {
+    let weight = arr[i - 1][0];
+    let value = arr[i - 1][1];
+
+    //물건을 배낭에 담을수 있을때
+    if (weight <= j) dp[i][j] = dp[i - 1][j - weight] + value;
+
+    // 현재 물건을 담은 가치보다 물건들의 가치가 큰지 확인해야함
+    dp[i][j] = Math.max(dp[i][j], dp[i - 1][j]);
+  }
 }
 
-//해당 풀이가 가능한 이유는 1번물품,2번물품을 지나면서 가방에 들어갈수 있는 최대 가치를 따지기때문에
-//마지막 물품을 평가했을때 가장 가치가 높게 계산된다.
-for(let i=1;i<=n;i++){
-    for(let j=0;j<=k;j++){
-
-        if(item[i-1].weight > j){
-            arr[i][j]=arr[i-1][j];
-            continue;
-        }
-
-        //배낭에 아이템을 넣을 수 있다면
-        //j만큼 무게를 넣었을때 j-item[i-1].weight만큼 넣었던 배낭이 있으면 더해준다.
-        arr[i][j]= arr[i-1][j-item[i-1].weight]+item[i-1].value;
-
-        //j만큼 무게를 넣은 가방에서 이전 조합이 더 가치가 클수 있다.
-        arr[i][j]= Math.max(arr[i][j],arr[i-1][j]);
-    }
+console.log(dp[n][k]);
+function print() {
+  for (let i = 0; i <= n; i++) {
+    console.log(dp[i].join(" "));
+  }
 }
-console.log(Math.max(...arr[n]));
